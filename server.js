c56856d6abcbd5425dbbc742076073e3c077546e
panda863 <142486802+panda863@users.noreply.github.com>
2023-09-21 16:01:44 +0900

    server.jsのlistening PORT部分を編集

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const server = http.createServer((req,res)=>{
     res.end();
 });
 
-server.listen(PORT,()=>{
-    console.log("server running!");
+const port = process.env.PORT || 8000;
+server.listen(port,()=>{
+    console.log(`Listening on port ${port}...`);
 });
